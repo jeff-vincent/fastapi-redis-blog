2621101cb2eff038cafd71f00e13af82ffe8048c
jeff.vincent <jeff.d.vincent@gmail.com>
2024-11-19 19:06:34 -0800

    Add stuff to react app

---
+++ frontend/src/components/homePage.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const Posts = () => {
+
+    // Fetching data from an API
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+           .then(response => response.json())
+           .then(data => setPosts(data))
+           .catch(error => console.error('Error:', error));
+    }, []);
+
+    console.log(posts);
+
+
+  return (
+    <div className="container">
+       <div>
+           {posts.map((post) => (
+               <div key={post.id}>
+                   <h2>{post.title}</h2>
+                   <p>{post.body}</p>
+               </div>
+           ))}
+       </div>
+    </div>
+  );
+};
+
+export default Posts;
